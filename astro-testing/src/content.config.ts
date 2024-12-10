75a2f13a11be59cd11d49215a77653c7634ada15
Nicholas Rothwell <n.rothwell@arts.ac.uk>
2024-12-10 17:06:18 +0000

    Loader glob fix.

--- astro-testing/src/content.config.ts
+++ astro-testing/src/content.config.ts
@@ -8,7 +8,7 @@ import { glob /*, file*/ } from 'astro/loaders'
 // NICK: not sure why this isn't in collections directory? Ah: that's deprecated,
 // it looks like feeds can be anywhere (files generally in src).
 const blog = defineCollection({
-    loader: glob({ pattern: ["*.md", "*.mdx"], base: "./src/data/blog" }),
+    loader: glob({ pattern: "*.{md,mdx}", base: "./src/data/blog" }),
     schema: z.object({
         title: z.string(),
         author: z.string()
